c350968553379e799a1e8915ec4b14392c5ef5a7
Thomas S <schwaab.t@gmail.com>
2020-11-25 14:30:24 +0100

    add modular layout system (e.g. menu at asstes/layout/navbar.php)

---
+++ src/assets/layout/login_popup.php
@@ -0,0 +1,27 @@
+  
+  <!--------Dingens HTML for Login Popup form--------->
+  
+	  
+	  <div id="id01" class="modal">
+  <form class="modal-content animate" action="./login.php" method="post">
+ 
+
+    <div class="container">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+        
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+
+    <div class="container" style="background-color:#727273">
+      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+      <span class="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form> 
+</div>
