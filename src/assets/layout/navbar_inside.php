c4f70be13cf88f9f274456ecc0434783645ca013
Thomas S <schwaab.t@gmail.com>
2020-12-13 20:27:49 +0100

    add dropdown for market

--- src/assets/layout/navbar_inside.php
+++ src/assets/layout/navbar_inside.php
@@ -1,3 +1,5 @@
+
+
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -8,8 +10,13 @@
       <li class="nav-item <?php echo (basename($_SERVER['SCRIPT_NAME']) == 'home.php') ? 'active' : '';?>">
         <a class="nav-link" href="./home.php">Home</a>
       </li>
-      <li class="nav-item <?php echo (basename($_SERVER['SCRIPT_NAME']) == 'market.php') ? 'active' : '';?>">
-        <a class="nav-link" href="./market.php">Marketplace</a>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Marketplace</a>
+		<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="./addOffer.php">Add</a>
+          <a class="dropdown-item" href="./market.php">View All</a>
+          <a class="dropdown-item" href="#">else here</a>
+        </div>
       </li>
       <li class="nav-item <?php echo (basename($_SERVER['SCRIPT_NAME']) == 'settings.php') ? 'active' : '';?>">
         <a class="nav-link" href="./settings.php">Settings</a>
