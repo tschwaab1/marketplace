decaea07d06ef0a846724cbcea750bce6295bcf1
tschwaab1 <schwaab.t@gmail.com>
2020-12-20 15:41:55 +0100

    delete unused constant

--- src/includes/config.php
+++ src/includes/config.php
@@ -4,11 +4,7 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'marketplace');
- 
-//Security Configuration
-// 1 -> enabled, 0 -> disabled
 
-define('ESCAPE_QUERY', 0);
  
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
